7a5601c8f268cd4b2481daf63f45eb80586206e9
weiyuemin <weiyuemin@163.com>
2014-09-25 14:47:50 +0800

    刚才的不对

--- protobuf/proto.sh
+++ protobuf/proto.sh
@@ -1,1 +1,1 @@
-..\3rdparty\protobuf\vsprojects\Debug\protoc --proto_path=./protos/$1 --cpp_out=./protos/out
+..\3rdparty\protobuf\vsprojects\Debug\protoc --proto_path=./protos ./protos/$1 --cpp_out=./protos/out
